cd248399a3109cff0e79f7293d9b3538fc26db16
Minori Tokuda <minori.tokuda@origami.com>
2019-07-06 15:16:06 +0900

    update v9

--- keyboards/zinc/keymaps/minoritea_v9/keymap.c
+++ keyboards/zinc/keymaps/minoritea_v9/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Lower| Ctrl | Alt  | Win  | Raise(Space)|             | Raise(Space)|  Win |  Alt | Ctrl |      |
+   * | Lower| Ctrl | Alt  | Win  | Raise(Space)|             | Raise(Space)|  Win |  Alt | Ctrl | Lower|
    * `-----------------------------------------'             `-----------------------------------------'
    */
 [_QWERTY] = LAYOUT_ortho_4x12(
       KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      LOWER,    KC_LCTL, KC_LALT, KC_LGUI, _______, RAISE,               RAISE,  _______, KC_RGUI, KC_RALT, KC_RCTL, _______ \
+      LOWER,   KC_LCTL, KC_LALT, KC_LGUI, _______, RAISE,               RAISE,  _______, KC_RGUI, KC_RALT, KC_RCTL, LOWER \
       ),
 
   /* Raise
@@ -52,19 +52,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 
   /* LOWER
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  | F10  |      |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  | F10  | Pause|
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |      |      |      |      |      |             | Home | PgDn | PgUp | End  | F11  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |      |      |      |      |      |             |      |      |      |      | F12  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |  Win+Space  |             |  Win+Space  |      |      |      | Pause|
+   * |      |      |      |      |  Win+Space  |             |  Win+Space  |      |      |      |      |
    * `-----------------------------------------'             `-----------------------------------------'
    */
   [_LOWER] = LAYOUT_ortho_4x12( \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PAUS, \
       _______, _______, _______, _______, _______, _______,             KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_F11,  _______, \
       _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, IM_TGL,              IM_TGL,  _______, _______, _______, _______, KC_PAUS \
+      _______, _______, _______, _______, _______, IM_TGL,              IM_TGL,  _______, _______, _______, _______, _______ \
       )
 };
